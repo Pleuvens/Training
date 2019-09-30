bcb733a5d15e61d7024fd33f598383638ccca69f
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2019-10-01 00:46:38 +0200

    [FLAT] - recursive done

--- src/flat.js
+++ src/flat.js
@@ -1,16 +1,29 @@
-function flat_iterative(array, depth=0) {
+function flat_iterative(array, depth=1) {
     let res = [];
     for (let item of array) {
         if (!Array.isArray(item)) {
             res.push(item);
             continue;
         }
+        let flatten_item = [];
+        let tmp_item = [...item];
+        for (let d = 0; tmp_item.length > 0 && d < depth; d++) {
+            let delete_list = [];
+            for (let elt of tmp_item)
+            {
+                if (!Array.isArray(elt))
+                    flatten_item.push(elt);
+            }
+            for (let elt of delete_list)
+                tmp_item.remove(elt);
+        }
+        res.concat(flatten_item);
     }
     return res;
 }
 
-function flat_recursive(array, depth=0) {
-    if (depth < 0)
+function flat_recursive(array, depth=1) {
+    if (depth <= 0)
         return array;
     let flatten = [];
     for (let item of array) {
--- tests/flat.test.js
+++ tests/flat.test.js
@@ -24,7 +24,86 @@ test('RECURSIVE: flatten array [0, 1, [2, [3, 4]]]', () => {
     expect(flat_recursive([0, 1, [2, [3, 4]]])).toEqual(array.flat());
 });
 
-test('RECURSIVE: flatten array [0, 1, 2, [3, 4]]', () => {
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]])).toEqual(array.flat());
+});
+
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]], 1)).toEqual(array.flat(1));
+});
+
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]], 2)).toEqual(array.flat(2));
+});
+
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]], 3)).toEqual(array.flat(3));
+});
+
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]], 4)).toEqual(array.flat(4));
+});
+
+test('RECURSIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_recursive([0, [1, [2, [3, [4]]]]], 5)).toEqual(array.flat(5));
+});
+
+test('ITERATIVE: flatten empty array', () => {
+    expect(flat_iterative([])).toEqual([]);
+});
+
+test('ITERATIVE: flatten array [0, 1, 2, [3, 4]]', () => {
+    let array = [0, 1, 2, [3, 4]];
+    expect(flat_iterative([0, 1, 2, [3, 4]])).toEqual(array.flat());
+});
+
+test('ITERATIVE: flatten array [[0, 1, 2], [3, 4]]', () => {
+    let array = [[0, 1, 2], [3, 4]];
+    expect(flat_iterative([[0, 1, 2], [3, 4]])).toEqual(array.flat());
+});
+
+test('ITERATIVE: flatten array [0, 1, 2, 3, 4]', () => {
+    let array = [0, 1, 2, 3, 4];
+    expect(flat_iterative([0, 1, 2, 3, 4])).toEqual(array.flat());
+});
+
+test('ITERATIVE: flatten array [0, 1, [2, [3, 4]]]', () => {
     let array = [0, 1, [2, [3, 4]]];
-    expect(flat_recursive([0, 1, [2, [3, 4]]], 2)).toEqual(array.flat(2));
+    expect(flat_iterative([0, 1, [2, [3, 4]]])).toEqual(array.flat());
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]])).toEqual(array.flat());
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]], 1)).toEqual(array.flat(1));
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]], 2)).toEqual(array.flat(2));
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]], 3)).toEqual(array.flat(3));
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]], 4)).toEqual(array.flat(4));
+});
+
+test('ITERATIVE: flatten array [0, [1, [2, [3, [4]]]]]', () => {
+    let array = [0, [1, [2, [3, [4]]]]];
+    expect(flat_iterative([0, [1, [2, [3, [4]]]]], 5)).toEqual(array.flat(5));
 });
